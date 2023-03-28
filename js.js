510564199da08c08db755d19a0682afde54497c4
Erik2406 <129183440+Erik2406@users.noreply.github.com>
2023-03-28 17:13:20 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,53 @@
+const gameBoard = document.querySelector('#gameboard');
+const infoDisplay = document.querySelector('#info');
+const startCalls = [
+    "","","","","","","","",""
+];
+
+let go = "circle";
+infoDisplay.textContent = "Circle goes first";
+
+function createBoard() {
+    startCalls.forEach((_cell, index) => {
+       const cellElement = document.createElement('div');
+       cellElement.classList.add('square');
+       cellElement.id = index;
+       gameBoard.append(cellElement);
+       cellElement.addEventListener('click', addGo);
+    })
+}
+createBoard();
+
+function addGo(e) {
+    const goDisplay = document.createElement('div');
+    goDisplay.classList.add(go);
+    e.target.append(goDisplay);
+    go = go === "circle" ? "cross": "circle";
+    infoDisplay.textContent = "it is now" + go +  "'s go.";
+    e.target.removeEventListener("click", addGo);
+    checkScore()
+}
+
+function checkScore() {
+  const allSquares = document.querySelectorAll(".square");
+    const winningCombos = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6],         
+    ];
+
+console.log(allSquares[0]);
+
+winningCombos.forEach(array => {
+    const crossWins = array.every(call =>
+         allSquares[call].firstChaild?.classList.contains('circle'));
+
+         if (crossWins) {
+            infoDisplay.textContent = "Circle Wins!";
+            allSquares.forEach(square =>scuare.replaceWith(square.cloneNode(true)));
+            return 
+         }
+});
+
+
+}
